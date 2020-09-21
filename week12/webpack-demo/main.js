bfa5154dabd515c988df4080fe217166814ca131
summerjixia <15921597850@163.com>
2020-09-21 12:39:38 +0800

    12周

--- week12/webpack-demo/main.js
+++ week12/webpack-demo/main.js
@@ -17,24 +17,56 @@ class Carousel extends Component {
             this.root.appendChild(child);
         }
 
-        let currentIndex = 0;
+        let position = 0;
+        this.root.addEventListener('mousedown', (event) => {
+            let children = this.root.children;
+            let startX = event.clientX;
+            let move = event => {
+                let x = event.clientX - startX;
+                let current = position - ((x - x % 500) / 500)
+                for (let offset of [-1, 0, 1]) {
+                    let pos = current + offset;
+                    pos = (pos + children.length) % children.length;
+                    children[pos].style.transition = "none";
+                    children[pos].style.transform = `translateX(${-position * 280 + offset * 280 + x % 280}px)`;
+                }
+
+            }
+            let up = event => {
+                let x = event.clientX - startX;
+                position = position - Math.round(x / 280);
+                for (let offset of [0, -Math.sign(Math.round(x / 280) - x + 140 * Math.sign(x))]) {
+                    let pos = position + offset;
+                    pos = (pos + children.length) % children.length;
+                    children[pos].style.transition = "";
+                    children[pos].style.transform = `translateX(${-position * 280 + offset * 280}px)`;
+                }
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            }
+
+            document.addEventListener("mousemove", move);
+            document.addEventListener("mouseup", up);
+
+        })
+        /*let currentIndex = 0;
         setInterval(() => {
             let children = this.root.children;
-            let nextIndex = (currentIndex+1) % children.length
+            let nextIndex = (currentIndex + 1) % children.length
 
             let current = children[currentIndex];
             let next = children[nextIndex];
 
             next.style.transition = "none";
             next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
-            
+
             setTimeout(() => {
-                next.style.transition = "";
-                current.style.transform = `translateX(${-100 -currentIndex * 100})`
-                next.style.transform = `translateX(${-nextIndex * 100})`;
+                next.style.transition = "";//让style标签的transition生效
+                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`
+                next.style.transform = `translateX(${-nextIndex * 100}%)`;
                 currentIndex = nextIndex;
             }, 16);
-        }, 3000);
+        }, 3000);*/
         return this.root;
     }
 
